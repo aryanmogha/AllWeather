a5acaf70306b10f2610caf76503df2b525d12365
Aryan <aryan1mogha@gmail.com>
2023-06-02 21:02:24 +0530

    Initial Commit

---
+++ AllWeather/AllWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllWeatherApp.swift
+//  AllWeather
+//
+//  Created by Aryan Mogha on 02/06/23.
+//
+
+import SwiftUI
+
+@main
+struct AllWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AllWeather/Views/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  AllWeather
+//
+//  Created by Aryan Mogha on 02/06/23.
+//
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject var locationManager = LocationManager()
+    
+    var body: some View {
+        VStack {
+            
+            if let location = locationManager.location {
+                Text("Your coordinates are: \(location.longitude), \(location.latitude)")
+            }
+            else{
+                if locationManager.isLoading {
+                    ProgressView()
+                }
+                else {
+                    WelcomeView().environmentObject(locationManager)
+                }
+            }
+            
+            
+        }
+        .background (Color (hue: 0.656, saturation: 0.787, brightness: 0.354))
+        .preferredColorScheme(.dark)
+    }
+}
+
+struct ContentView_Previews:
+    PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
